f1b8d46abab694c9f9b0fe2099605c9c6c11fc77
Renato Chocce Palacios <renatochocce758@gmail.com>
2025-01-24 16:21:00 -0300

    agregando el nav de bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import NavBar from "./components/NavBar/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Importa el JS de Bootstrap
+
 
 function App() {
     return (
         <div>
             <NavBar />
 
-            <div className="container">
+            {/* <div className="container">
                 <h1 className="text-center">Hola, Bootstrap en React</h1>
                 <button className="btn btn-primary">Botón</button>
-            </div>
+            </div> */}
         </div>
     );
 }
