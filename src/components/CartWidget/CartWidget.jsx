88fb39341068f9e0a1c5ea9c082e754fe39882f9
Renato Chocce Palacios <renatochocce758@gmail.com>
2025-01-28 17:02:29 -0300

    agregando una libreria y modificando estilos

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,13 +1,12 @@
 
+import { GrCart } from "react-icons/gr";
 
 function CartWidget() {
-    const contador = 0;
+    const contador = 20;
 
     return (
         <button className="cart-widget">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 icon" >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-            </svg>
+            <GrCart className="cart-icon"/>
             <div className="cantidad-cart">{contador}</div>
         </button>
     )
